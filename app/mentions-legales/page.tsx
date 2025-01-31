b97bf4cc3fce3638e42f7812b94f9a44a7d4fdcc
odakris <christopher_denis@live.fr>
2025-01-31 15:07:23 +0100

    create mention legales page

---
+++ app/mentions-legales/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import MentionLegales from "@/components/Footer/MentionLegales"
+
+export default function MentionLegalesPage() {
+  return (
+    <div className="flex justify-center items-center h-[100%]">
+      <MentionLegales />
+    </div>
+  )
+}
