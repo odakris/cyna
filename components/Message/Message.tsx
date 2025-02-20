181464fb978ce429a4a70f0714fd039114ff2361
odakris <christopher_denis@live.fr>
2025-02-20 11:21:21 +0100

    update message component to handle custom messages

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,11 +1,11 @@
 import { Separator } from "@/components/ui/separator"
 
-export function Message() {
+export function Message({ message }: { message: string }) {
   return (
     <div className="bg-gray-100 p-4 rounded-lg shadow-md">
       <div className="space-y-1">
         <h1 className="text-2xl font-bold text-center text-primary">
-          Message très très important sur les promotions et actualités
+          {message}
         </h1>
       </div>
       <Separator className="my-4" />
