fe9571338ac0e6cd4c0c8a4807d7222cf3d356a9
odakris <christopher_denis@live.fr>
2025-02-20 09:19:00 +0100

    add linkedin link in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link"
 import React from "react"
+import { Linkedin } from "lucide-react"
 
 export function Footer() {
   return (
     <div className="cyna-bg-primary-color">
-      <div className="max-w-7xl mx-auto px-10 py-4 w-full underline hover:no-underline cyna-text-color">
+      <div className="flex justify-between max-w-7xl mx-auto px-10 py-4 w-full underline hover:no-underline cyna-text-color">
         <Link href="./mentions-legales">Mentions Légales</Link>
+        <Link href="https://www.linkedin.com/company/cyna-it/" target="_blank">
+          <Linkedin />
+        </Link>
       </div>
     </div>
   )
