419fdb6b78fa3517188f75e1d8ca57a2a4bc75e5
odakris <christopher_denis@live.fr>
2025-03-09 17:52:11 +0100

    Create api to get all categories

---
+++ app/api/categories/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+  try {
+    const categories = await prisma.categorie.findMany({})
+
+    return new NextResponse(JSON.stringify(categories ?? {}), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error) {
+    console.error("Erreur lors de la recherche des categories :", error)
+    return NextResponse.json({ message: "Erreur serveur" }, { status: 500 })
+  }
+}
