7b37900a345b913ac28ce87640759d7a4801d40e
Hugo Zins <h.zins@lprs.fr>
2025-03-23 17:28:13 +0100

    Modif Avatar component statut connexion

--- app/produit/[id]/page.tsx
+++ app/produit/[id]/page.tsx
@@ -15,8 +15,7 @@ const ProductPage = () => {
   const [product, setProduct] = useState<ProductType>()
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
-  const [selectedSubscription, setSelectedSubscription] =
-    useState<string>("mensuel") // Par défaut, sélection mensuelle
+  const [selectedSubscription, setSelectedSubscription] = useState<string>("mensuel") // Par défaut, sélection mensuelle
 
   // Référence pour faire défiler vers le tableau tarifaire
   const pricingTableRef = useRef<HTMLTableElement | null>(null)
@@ -51,7 +50,7 @@ const ProductPage = () => {
 
   // Fonction pour gérer l'ajout au panier avec l'abonnement sélectionné
   const handleAddToCart = (subscriptionType: string) => {
-    if (!product) return
+    if (!product || !product.available) return // Ne pas ajouter au panier si le produit est indisponible
 
     addToCart({
       id: product.id_product,
@@ -72,11 +71,9 @@ const ProductPage = () => {
     pricingTableRef.current?.scrollIntoView({ behavior: "smooth" })
   }
 
-  // Prix fixe pour tous les produits
   const prixMensuel = "49.99"
   const prixAnnuel = "499.90"
 
-  // Prix par utilisateur et appareil
   const prixUnitaire = product?.unit_price
   const prixParAppareil = 19.99 // Exemple de prix par appareil
 
@@ -122,7 +119,7 @@ const ProductPage = () => {
             {loading ? (
               <Skeleton className="w-1/2 h-6 mx-auto" />
             ) : (
-              "Disponible immédiatement"
+              product?.available ? "Disponible immédiatement" : "Service Indisponible"
             )}
           </h2>
 
@@ -130,114 +127,124 @@ const ProductPage = () => {
             <Skeleton className="w-full h-[60%] rounded-lg" />
           ) : (
             <div className="flex justify-center w-full">
-              {" "}
-              {/* Assure que le bouton est bien centré */}
-              <Button
-                className="w-auto py-2 text-lg" // Réduit la taille du bouton ici
-                variant="cyna"
-                onClick={handleScrollToPricingTable} // Ajout du scroll ici
-              >
-                S&apos;ABONNER MAINTENANT
-              </Button>
+              {product?.available ? (
+                <Button
+                  className="w-auto py-2 text-lg" // Réduit la taille du bouton ici
+                  variant="cyna"
+                  onClick={handleScrollToPricingTable} // Ajout du scroll ici
+                >
+                  S&apos;ABONNER MAINTENANT
+                </Button>
+              ) : (
+                <Button
+                  className="w-auto py-2 text-lg"
+                  variant="cyna"
+                  disabled
+                >
+                  SERVICE INDISPONIBLE
+                </Button>
+              )}
             </div>
           )}
         </div>
       </section>
 
       {/* Section des informations tarifaires - Tableau simplifié */}
-      <section className="py-8 px-6 bg-white" ref={pricingTableRef}>
-        <h2 className="text-2xl font-bold text-center text-gray-900 mb-6">
-          Choisir une formule d&apos;abonnement
-        </h2>
+      {product?.available && (
+        <section className="py-8 px-6 bg-white" ref={pricingTableRef}>
+          <h2 className="text-2xl font-bold text-center text-gray-900 mb-6">
+            Choisir une formule d&apos;abonnement
+          </h2>
 
-        <div className="overflow-x-auto">
-          <table className="w-full max-w-screen-lg mx-auto table-auto border-collapse border border-gray-300">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="px-4 py-2 text-center w-1/3">
-                  Modèle Tarifaire
-                </th>
-                <th className="px-4 py-2 text-center w-1/3">Prix</th>
-                <th className="px-4 py-2 text-center w-1/3">
-                  Ajouter au Panier
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="border px-4 py-2 text-center">Coût unitaire</td>
-                <td className="border px-4 py-2 text-center">
-                  {prixUnitaire} €
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  <Button
-                    onClick={() => handleAddToCart("unitaire")}
-                    className="w-auto py-1 text-sm"
-                    variant="cyna"
-                  >
-                    Ajouter au panier
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border px-4 py-2 text-center">
-                  Abonnement Mensuel
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  {prixUnitaire} € puis
-                  <br /> {prixMensuel}€ / mois
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  <Button
-                    onClick={() => handleAddToCart("mensuel")}
-                    className="w-auto py-1 text-sm"
-                    variant="cyna"
-                  >
-                    Ajouter au panier
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border px-4 py-2 text-center">
-                  Abonnement Annuel
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  {prixUnitaire} € puis <br /> {prixAnnuel}€ / an (dont 2 mois
-                  offerts)
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  <Button
-                    onClick={() => handleAddToCart("annuel")}
-                    className="w-auto py-1 text-sm"
-                    variant="cyna"
-                  >
-                    Ajouter au panier
-                  </Button>
-                </td>
-              </tr>
-              {/* Prix par appareil */}
-              <tr>
-                <td className="border px-4 py-2 text-center">
-                  Coût par Appareil
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  {prixUnitaire} € puis <br /> {prixParAppareil}€ / appareil
-                  supplémentaire
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  <Button
-                    onClick={() => handleAddToCart("appareil")}
-                    className="w-auto py-1 text-sm"
-                    variant="cyna"
-                  >
-                    Ajouter au panier
-                  </Button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
+          <div className="overflow-x-auto">
+            <table className="w-full max-w-screen-lg mx-auto table-auto border-collapse border border-gray-300">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="px-4 py-2 text-center w-1/3">
+                    Modèle Tarifaire
+                  </th>
+                  <th className="px-4 py-2 text-center w-1/3">Prix</th>
+                  <th className="px-4 py-2 text-center w-1/3">
+                    Ajouter au Panier
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border px-4 py-2 text-center">Coût unitaire</td>
+                  <td className="border px-4 py-2 text-center">
+                    {prixUnitaire} €
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    <Button
+                      onClick={() => handleAddToCart("unitaire")}
+                      className="w-auto py-1 text-sm"
+                      variant="cyna"
+                    >
+                      Ajouter au panier
+                    </Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-2 text-center">
+                    Abonnement Mensuel
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    {prixUnitaire} € puis
+                    <br /> {prixMensuel}€ / mois
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    <Button
+                      onClick={() => handleAddToCart("mensuel")}
+                      className="w-auto py-1 text-sm"
+                      variant="cyna"
+                    >
+                      Ajouter au panier
+                    </Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-2 text-center">
+                    Abonnement Annuel
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    {prixUnitaire} € puis <br /> {prixAnnuel}€ / an (dont 2 mois
+                    offerts)
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    <Button
+                      onClick={() => handleAddToCart("annuel")}
+                      className="w-auto py-1 text-sm"
+                      variant="cyna"
+                    >
+                      Ajouter au panier
+                    </Button>
+                  </td>
+                </tr>
+                {/* Prix par appareil */}
+                <tr>
+                  <td className="border px-4 py-2 text-center">
+                    Coût par Appareil
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    {prixUnitaire} € puis <br /> {prixParAppareil}€ / appareil
+                    supplémentaire
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    <Button
+                      onClick={() => handleAddToCart("appareil")}
+                      className="w-auto py-1 text-sm"
+                      variant="cyna"
+                    >
+                      Ajouter au panier
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      )}
 
       {/* Similar Products Section */}
       <section className="py-12 px-6">
